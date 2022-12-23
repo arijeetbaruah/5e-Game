20f6c10c9d778c4190553aac631aeec649f39b69
gameshastraeng2 <gameshastraeng2@jamcity.com>
2022-12-23 11:53:38 +0530

    spell scripts

---
+++ Assets/Game/SO/SpellRegistery.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+using Sirenix.Utilities;
+
+namespace Game.SO
+{
+    [GlobalConfig("Assets/Game/Resources/")]
+    public class SpellRegistery : GlobalConfig<SpellRegistery>
+    {
+    }
+}
